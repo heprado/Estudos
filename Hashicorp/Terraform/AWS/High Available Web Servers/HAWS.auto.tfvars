087c76578bea618b5fa4449b8a83eecb614534a3
Tidebinder <lipnose1@gmail.com>
2021-02-02 16:43:14 -0300

    020221

---
+++ Hashicorp/Terraform/AWS/High Available Web Servers/HAWS.auto.tfvars
@@ -0,0 +1,5 @@
+provider_config = {
+    "region" = "us-east-2"
+    "credentials_path" = "./credentials"
+    "profile" = "default"
+}
--- Hashicorp/Terraform/AWS/High Available Web Servers/variables.tf
+++ Hashicorp/Terraform/AWS/High Available Web Servers/variables.tf
@@ -8,9 +8,19 @@ variable "provider_config" {
 }
 
 variable prefix_name {
+    /*Prefixo que será usado para criação de objetos na AWS*/
     default = "HAWS_"
 }
 
 variable vpc_cidr_block {
+    /*CIDR que será usada para criação de subnets dentro do VPC*/
     default = "192.168.0.0/16"
 }
+
+variable subnets {
+    /*Subnets que serão criadas dentro do VPC*/
+    type = list
+    default = ["192.168.1.0/24","192.168.2.0/24"]
+}
+
+
