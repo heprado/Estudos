ce886435c4fe6908795500531da77a9fc07e4714
Henrique Prado de Sousa <henriqueprado1997@hotmail.com>
2024-10-17 18:51:21 -0300

    Codigo pra escrever todas as letras de todas as palavras em varias linhas.

---
+++ C/letras_em_linhas/letras_em_linhas.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define ESPACO 32
+#define TAB 9
+#define NOVA_LINHA 10
+#define SEPARADOR_ENCONTRADO 0
+#define PALAVRA_ENCONTRADA 1
+
+int main () {
+
+    int caracter;
+    
+    int palavra = SEPARADOR_ENCONTRADO;
+
+
+    printf("Escreva palavras e aperte ENTER, vamos mostrar cada letra em uma linha:\n");
+    
+    printf("Aperta CTRL + C para sair.\n");
+
+
+    while ((caracter = getchar()) != EOF) {
+        
+        if (caracter == TAB ||
+            caracter == ESPACO ||
+            caracter == NOVA_LINHA) {
+                palavra = SEPARADOR_ENCONTRADO;
+            }
+        else if (palavra == SEPARADOR_ENCONTRADO) {
+            palavra = PALAVRA_ENCONTRADA;
+            
+        };
+        
+        if(palavra == PALAVRA_ENCONTRADA) {
+            
+            putchar(caracter);
+            putchar(NOVA_LINHA);
+        };
+
+        
+    }
+    
+    
+
+    return 0;
+}
