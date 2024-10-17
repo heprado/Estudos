e1db1f3ae8b5076e543598cedf990a5980a1a6b4
Henrique Prado de Sousa <henriqueprado1997@hotmail.com>
2024-10-17 18:53:51 -0300

    Codigo para contar quantas palavras tem em um texto.

--- C/contar_palavras/contar_palavras.c
+++ C/contar_palavras/contar_palavras.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
-#define SPACE 32
+#define ESPACO 32
 #define TAB 9
-#define NL 10
-#define OUTSIDE_WORD 0 
-#define INSIDE_WORD 1
+#define NOVA_LINHA 10
+#define SEPARADOR_ENCONTRADO 0 
+#define PALAVRA_ENCONTRADA 1
 
 int main () {
 
-    int c;
+    int caracter;
+
     int wc; 
     
-    int word = OUTSIDE_WORD;
+    int palavra = SEPARADOR_ENCONTRADO;
 
     printf("Escreva palavras e aperte  \e[1;32mCTRL + C \e[0m para contarmos quantas palavras tem:\n");
 
-    while ((c = getchar()) != EOF) {
+    while ((caracter = getchar()) != EOF) {
 
-        if (c == TAB ||
-            c == SPACE ||
-            c == NL) {
-                word = OUTSIDE_WORD;
+        if (caracter == TAB ||
+            caracter == ESPACO ||
+            caracter == NOVA_LINHA) {
+                palavra = SEPARADOR_ENCONTRADO;
             }
         
-        else if ( word == OUTSIDE_WORD) {
-            word = INSIDE_WORD;
+        else if ( palavra == SEPARADOR_ENCONTRADO) {
+            palavra = PALAVRA_ENCONTRADA;
             wc++;
         }
     }
