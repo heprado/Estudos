b664cf2999e98cfc3aba362d05c43602101f13d9
Henrique Prado de Sousa <henriqueprado1997@hotmail.com>
2024-10-17 18:57:23 -0300

    typo =(

--- C/eco/eco.c
+++ C/eco/eco.c
@@ -5,7 +5,7 @@ int main() {
     int c;
 
     /*Recebe o caracter digitado e checa se ele é -1(EOF)*/
-    /*EOF é uma costante da biblioteca stdio que define o fim de um arquivo que no nosso caso é quando pressionamos enter.*/
+    /*EOF é uma constante da biblioteca stdio que define o fim de um arquivo que no nosso caso é quando pressionamos enter.*/
 
     while ((c = getchar()) != EOF) {
         /*Imprime os caracteres recebidos*/
