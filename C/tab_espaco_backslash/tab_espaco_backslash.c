42c29df78e83d85208e5301ddb7f3a0a528b0b90
Henrique Prado de Sousa <henriqueprado1997@hotmail.com>
2024-10-17 19:16:40 -0300

    mudando umas parada.

--- C/tab_espaco_backslash/tab_espaco_backslash.c
+++ C/tab_espaco_backslash/tab_espaco_backslash.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#define SPACE 32
+#define ESPACO 32
 #define TAB 9
-#define BACKSPACE 8
-#define BACKSLASH 92
+#define VOLTAR 8
+#define BARRA_ESQUERDA 92
 
-//Não deu certo com backspace como pede no livro, eu precisaria ou da curses(linux) ou da conio(windows) para executar ai eu só decidi não fazer mesmo de desgosto, mas depois eu vou tentar fazer uma versão que funciona pros dois usando uns ifndef sei la uns negocio assim eu preciso entender ainda...
+//Não deu certo com VOLTAR como pede no livro, eu precisaria ou da curses(linux) ou da conio(windows) para executar ai eu só decidi não fazer mesmo de desgosto, mas depois eu vou tentar fazer uma versão que funciona pros dois usando uns ifndef sei la uns negocio assim eu preciso entender ainda...
 
 int main () {
 
@@ -14,17 +14,17 @@ int main () {
     while ((c = getchar()) != EOF) {
 
         switch (c) {
-            case SPACE:
-                putchar(BACKSLASH);
+            case ESPACO:
+                putchar(BARRA_ESQUERDA);
                 putchar('s');
                 continue;
             case TAB:
-                putchar(BACKSLASH);
+                putchar(BARRA_ESQUERDA);
                 putchar('t');
                 continue;
-            case BACKSLASH:
-                putchar(BACKSLASH);
-                putchar(BACKSLASH);
+            case BARRA_ESQUERDA:
+                putchar(BARRA_ESQUERDA);
+                putchar(BARRA_ESQUERDA);
                 continue;
         }
         putchar(c);
