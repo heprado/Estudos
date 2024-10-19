151af4ded3020bd73e3ace93aa9cda571d2e40a6
Henrique Prado de Sousa <henriqueprado1997@hotmail.com>
2024-10-19 09:20:23 -0300

    Desisti após tentar por dois dias printar um caracter unicode no terminal.

--- C/palavras_em_linhas/p.c
+++ C/palavras_em_linhas/p.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+
+#include <locale.h>
+
+
 #define ESPACO 32
 #define TAB 9
 #define NOVA_LINHA 10
 #define SEPARADOR_ENCONTRADO 0
-#define PALAVRA_ENCONTRADA 1
+#define LETRA_ENCONTRADA 1
 
 int main () {
 
+    setlocale(LC_ALL,"");
+    
     int caracter;
     
     int palavra = SEPARADOR_ENCONTRADO;
@@ -17,8 +23,8 @@ int main () {
     printf("Aperte \e[1;32m CTRL + C \e[0m para sair.\n");
     printf("Não tá pronto...\n");
 
+
     while ((caracter = getchar()) != EOF) {
-        
         
         if (caracter == TAB ||
             caracter == ESPACO ||
@@ -26,19 +32,21 @@ int main () {
              palavra = SEPARADOR_ENCONTRADO;
             }
         else if (palavra == SEPARADOR_ENCONTRADO) {
-         palavra = PALAVRA_ENCONTRADA;
+            palavra = LETRA_ENCONTRADA;   
+        };
+        
+        if (palavra == LETRA_ENCONTRADA) {
             
-        };
-        
-        if (palavra == PALAVRA_ENCONTRADA) {
             putchar(caracter);
+        } else if (palavra == SEPARADOR_ENCONTRADO)  {
             putchar(NOVA_LINHA);
-        };
+        }
+            
+    
 
-        
     }
     
-    
+ 
 
     return 0;
 }
