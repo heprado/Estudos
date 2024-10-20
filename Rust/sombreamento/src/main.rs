032de5194f9bd03dfd2a293c28bb45f3094094af
Henrique Prado de Sousa <henriqueprado1997@hotmail.com>
2024-10-20 19:54:04 -0300

    Adicionando outro exemplo usando um if para criar um novo escopo condicionalmente.

--- Rust/sombreamento/src/main.rs
+++ Rust/sombreamento/src/main.rs
@@ -1,7 +1,5 @@
 
-
-fn main() {
-
+fn novo_escopo() {
     //Ponteiro imutável(Não pode ser outra string em tempo de execução)
     //para uma string.
     let nome:& str = "Henrique";
@@ -18,4 +16,35 @@ fn main() {
         //Imprimindo no STDIN o novo nome dentro da variavel nome
         println!("Nome no escopo 1 {}", nome);
     }
+}
+
+fn if_novo_escopo() {
+
+    //Ponteiro imutável(Não pode ser outra string em tempo de execução)
+    //para uma string.
+    let nome:&str = "Henrique";
+
+    //Imprimindo no STDIN o nome dentro da variável nome
+    println!("Nome no escopo {}", nome);
+
+    //Criando um novo escopo caso o nome seja Henrique.
+    if nome == "Henrique" {
+
+        //Criando outro ponteiro imutável para uma string, utilizando o mesmo nome de variável.
+        //Isso só é possivel pois estamos em outro escopo.
+        //O interessante aqui é que estamos dentro de um escopo contextual,
+        // podemos criar um escopo diferente para cada valor atribuido a variavel quando ela for inicializada.
+        let nome:&str = "Sombra Henrique";
+        println!("Nome dentro do escopo IF {}", nome);
+    }
+
+}
+
+fn main() {
+
+    println!("=========================================================");
+    novo_escopo();
+    println!("=========================================================");
+    if_novo_escopo();
+    println!("=========================================================");
 }
