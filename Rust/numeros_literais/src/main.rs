484e1b2ecbc210885c6aa97ab85aecf13213182f
heprado <lipnose1@gmail.com>
2024-10-22 13:35:51 -0300

    Tipos de números literais.

---
+++ Rust/numeros_literais/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+    let decimal:u32 = 2_5_5;
+
+    let octal = 0o377;
+
+    let hexadecimal = 0xFF;
+
+    let binario = 0b1111_1111;
+
+    let byte = b'0';
+
+    println!("{}\n\
+    {}\n\
+    {}\n\
+    {}\n\
+    {}",decimal,octal,hexadecimal,binario,binario);
+
+}
